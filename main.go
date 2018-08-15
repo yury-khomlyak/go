0cd216935cb25e12b631fc49a59353136529691a
Vitaly Kezlya <vitaly@kezlya.com>
2018-08-14 22:18:11 -0600

    Disable sleep and applying Random Actions

--- main.go
+++ main.go
@@ -19,21 +19,11 @@ func whatToDo(hive *Hive) ActDir {
 	rand.Seed(time.Now().UnixNano())
 
 	for id := range hive.Ants {
-
-		//Only move or Eat randomly
-		act := Action(0)
-		switch rand.Intn(2) {
-		case 0:
-			act = Move
-		case 1:
-			act = Eat
-		}
-
-		//                       Random direction
-		actions[id] = int(act) + rand.Intn(4)
+		//            Random Action     Random direction
+		actions[id] = rand.Intn(5)*10 + rand.Intn(4)
 	}
 
-	time.Sleep(20 * time.Millisecond)
+	//time.Sleep(20 * time.Millisecond)
 
 	return actions
 }
